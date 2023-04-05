85f9737af6c0181c3e4d94bb55e06e9c44abd6db
Tudor Iovita <iovitatudor21@gmail.com>
2023-04-05 13:59:48 +0300

    Fix bacend url path

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const urlParams = new URLSearchParams(queryString);
 const token = urlParams.get('token')
 // const backendUrl = 'http://127.0.0.1:5000/api';
 // const basicToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjE0NDIwNzcyNjE4OSwiaWF0IjoxNjc1NzAwNzcxLCJleHAiOjE2NzU3ODcxNzF9.zFu7t-3xeVH5iWAK02BKG2tiX65TmDXcf3qMHlqWLAo';
-const backendUrl = 'https://core.instantexpert.online';
+const backendUrl = 'https://core.instantexpert.online/api';
 const basicToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSW5zdGFudGV4cGVydCIsInVybCI6Imh0dHBzOi8vaW5zdGFudGV4cGVydC5vbmxpbmUvIiwidW5pcXVlSWQiOjgxMjczNDUzODQyNCwiaWF0IjoxNjc4ODc2NzgwLCJleHAiOjE2Nzg5NjMxODB9.6nvMpMRB_QsaHuGSePWj7EU4KDcRSRki2xkGT7IDFU8';
 
 
