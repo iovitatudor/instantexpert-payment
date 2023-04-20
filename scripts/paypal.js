3fc5a283bf13f6a7b39859de2ab343d8131e5467
Tudor Iovita <iovitatudor21@gmail.com>
2023-04-20 10:56:36 +0300

    Change currency code

--- scripts/paypal.js
+++ scripts/paypal.js
@@ -2,6 +2,7 @@ paypal.Buttons({
   // Order is created on the server and the order id is returned
   createOrder: (data, actions) => {
     const createdOrder = JSON.parse(localStorage.getItem('order'));
+    console.log('lvfmdlmvdf');
     return fetch(`https://core.instantexpert.online/api/payments/order/${createdOrder.id}?lang=ro`, {
       method: "post",
       headers: {
@@ -27,7 +28,7 @@ paypal.Buttons({
       .then((response) => response.json())
       .then((orderData) => {
         // Successful capture! For dev/demo purposes:
-        // console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+        console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
         const transaction = orderData.purchase_units[0].payments.captures[0];
         // alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
         // When ready to go live, remove the alert and show a success message within this page. For example:
@@ -35,7 +36,7 @@ paypal.Buttons({
         element.innerHTML = '<h3>Thank you for your payment!</h3>';
         const order = JSON.parse(localStorage.getItem('order'));
         // console.log(order);
-        window.location.href = `https://instantexpert.online/ro/callback/${order.expert.id}`;
+        // window.location.href = `https://instantexpert.online/ro/callback/${order.expert.id}`;
         // Or go to another URL:  actions.redirect('thank_you.html');
       });
   }
